7cb28a24f5aac3dd4cf0ddd78396c613f4dd194c
veprogames <veprogames.gamedev@gmail.com>
2022-09-19 19:35:17 +0200

    add dynamic places helper format_mantissa min places zero

--- notations/notation.lua
+++ notations/notation.lua
@@ -27,13 +27,19 @@ function Notation:format(n, places, places1000)
         p = places1000 or 0
     end
 
-    if self.dynamic then p = p - (n.e % 3) else end
+    if self.dynamic then p = p + self:dp(n) else end
 
     return self:get_prefix(n) .. self:get_number(n, p) .. self:get_suffix(n)
 end
 
+-- get dynamic places, added to base precision
+function Notation:dp(n)
+    return -(n.e % 3)
+end
+
 -- wrapper function to format numbers
 function Notation:format_mantissa(number, places)
+    places = math.max(0, places)
     return string.format("%."..places.."f", number)
 end
 
