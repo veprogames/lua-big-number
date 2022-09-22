bc163bd5707c84589651592324c59bf16c97faca
veprogames <veprogames.gamedev@gmail.com>
2022-09-22 21:00:27 +0200

    bug fix, remove unused variable

--- notations/thousandnotation.lua
+++ notations/thousandnotation.lua
@@ -14,8 +14,14 @@ end
 function ThousandNotation:get_number(n, places)
     local raw = self.format_mantissa(n:to_number(), places)
     local result = ""
-    local comma, _ = string.find(raw, "%.") - 1
-    if comma == nil then comma = #raw end
+    local comma = string.find(raw, "%.")
+
+    if comma == nil then
+        comma = #raw
+    else
+        comma = comma - 1
+    end
+
     for i = 1, #raw do
         result = result .. string.sub(raw, i, i)
         if (comma - i) % 3 == 0 and i > 1 and i < comma then
