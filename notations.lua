bac098379b673c72ba4431166444d479473698be
veprogames <veprogames.gamedev@gmail.com>
2022-10-30 09:28:28 +0100

    add standard notation

--- notations.lua
+++ notations.lua
@@ -6,6 +6,8 @@ Notations = {
     HebrewNotation = dofile("notations/hebrewnotation.lua"),
     ScientificNotation = dofile("notations/scientificnotation.lua"),
     EngineeringNotation = dofile("notations/engineeringnotation.lua"),
+    BaseStandardNotation = dofile("notations/basestandardnotation.lua"),
+    StandardNotation = dofile("notations/standardnotation.lua"),
     ThousandNotation = dofile("notations/thousandnotation.lua"),
     DynamicNotation = dofile("notations/dynamicnotation.lua")
 }
